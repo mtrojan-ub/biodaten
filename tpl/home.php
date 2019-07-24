9d7a7d53ad98f19fa9647e14b65844ab21da403d
Mario Trojan <mario.trojan@uni-tuebingen.de>
2019-07-24 16:56:07 +0200

    responsive jumbotron image

--- tpl/home.php
+++ tpl/home.php
@@ -2,7 +2,7 @@
 <section class="jumbotron text-center">
     <div class="container">
         <p>
-            <img src="img/biodaten_logo.jpg" alt="BioDATEN" height="400" />
+            <img id="img-home" src="img/biodaten_logo.jpg" alt="BioDATEN" />
         </p>
         <h1 class="jumbotron-heading">Bioinformatics DATa Environment</h1>
         <h2 class="jumbotron-heading">Science Data Center</h1>
