e4e183c97920336f51d055fb2056793f5d950e2b
Mario Trojan <mario.trojan@uni-tuebingen.de>
2019-07-30 13:50:30 +0200

    conferences by obxga01

--- tpl/conferences.php
+++ tpl/conferences.php
@@ -1,6 +1,9 @@
 <div class="container">
     <h1>Conferences</h1>
     <p>
-        ... upcoming conferences
+        A list of conferences and events organised by or related to <a target="_blank" href="https://elixir-europe.org/">ELIXIR</a> is <a target="_blank" href="https://elixir-europe.org/events">here</a>.
+    </p>
+    <p>
+        A list of conferences and events organised by or related to <a target="_blank" href="https://galaxyproject.org/events/">Galaxy</a> is <a target="_blank" href="https://galaxyproject.org/events/">here</a>.
     </p>
 </div>
