596cd315394ea2b6982da8c5d698aac275118699
Mario Trojan <mario.trojan@uni-tuebingen.de>
2019-09-26 14:18:36 +0200

    added conferences in september

--- tpl/conferences.php
+++ tpl/conferences.php
@@ -1,9 +1,15 @@
 <div class="container">
     <h1>Conferences</h1>
     <p>
-        A list of conferences and events organised by or related to <a target="_blank" href="https://elixir-europe.org/">ELIXIR</a> is <a target="_blank" href="https://elixir-europe.org/events">here</a>.
+        From 2019-09-23 to 2019-09-27: <a target="_blank" href="//galaxyproject.eu/event/2019-05-08-GalaxyHTS.Freiburg.September2019/">Galaxy HTS data analysis workshop</a>
     </p>
     <p>
-        A list of conferences and events organised by or related to <a target="_blank" href="https://galaxyproject.org/events/">Galaxy</a> is <a target="_blank" href="https://galaxyproject.org/events/">here</a>.
+        From 2019-09-30 to 2019-10-01: <a target="_blank" href="//galaxyproject.eu/event/2019-08-19-bioconda-cofest/">Bioconda Collaboration Fest</a>
+    </p>
+    <p>
+        A list of other conferences and events organised by or related to <a target="_blank" href="//elixir-europe.org/">ELIXIR</a> is <a target="_blank" href="https://elixir-europe.org/events">here</a>.
+    </p>
+    <p>
+        A list of other conferences and events organised by or related to <a target="_blank" href="//galaxyproject.org/events/">Galaxy</a> is <a target="_blank" href="https://galaxyproject.org/events/">here</a>.
     </p>
 </div>
