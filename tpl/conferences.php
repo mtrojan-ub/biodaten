25a9d3202e7304051088cac70686a703d7e4e5e5
Mario Trojan <mario.trojan@uni-tuebingen.de>
2020-06-02 12:00:10 +0200

    conferences.php: h2

--- tpl/conferences.php
+++ tpl/conferences.php
@@ -1,5 +1,11 @@
 <div class="container">
     <h1>Conferences</h1>
+    <h2>2020</h2>
+    <p>
+        From 2020-07-13 to 2020-07-17: Online-only workshop and hackathon on <a href="//indico.dkfz.de/event/225/">Nextflow</a> and <a href="//nf-co.re/events/2020/hackathon-july-2020">nf-core</a>
+    </p>
+
+    <h2>2019</h2>
     <p>
         From 2019-09-23 to 2019-09-27: <a target="_blank" href="//galaxyproject.eu/event/2019-05-08-GalaxyHTS.Freiburg.September2019/">Galaxy HTS data analysis workshop</a>
     </p>
@@ -9,9 +15,8 @@
     <p>
         From 2019-12-02 to 2019-12-04: ELEXIR-NL / Technical aspects of Data Stewardship (save the date)
     </p>
-    <p>
-        From 2020-07-13 to 2020-07-17: Online-only workshop and hackathon on <a href="//indico.dkfz.de/event/225/">Nextflow</a> and <a href="//nf-co.re/events/2020/hackathon-july-2020">nf-core</a>
-    </p>
+
+    <h2>Elixir &amp; Galaxy</h2>
     <p>
         A list of other conferences and events organised by or related to <a target="_blank" href="//elixir-europe.org/">ELIXIR</a> is <a target="_blank" href="https://elixir-europe.org/events">here</a>.
     </p>
