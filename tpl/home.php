fb42c8458c93ca0d48225a4de6ce17228641602b
Mario Trojan <mario.trojan@uni-tuebingen.de>
2020-06-02 10:37:10 +0200

    home.php: Bioinformatics DATa Environment => Bioinformatics DATa ENvironment

--- tpl/home.php
+++ tpl/home.php
@@ -4,7 +4,7 @@
         <p>
             <img id="img-home" src="img/biodaten_logo.jpg" alt="BioDATEN" />
         </p>
-        <h1 class="jumbotron-heading">Bioinformatics DATa Environment</h1>
+        <h1 class="jumbotron-heading">Bioinformatics DATa ENvironment</h1>
         <h2 class="jumbotron-heading">Science Data Center</h1>
     </div>
 </section>
